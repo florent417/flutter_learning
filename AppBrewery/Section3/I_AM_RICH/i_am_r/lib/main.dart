98aa04f6625df21953721eb44f59ddf017ef85e7
florent417 <flole@live.dk>
2020-05-17 10:54:25 +0200

    Started on images

--- AppBrewery/Section3/I_AM_RICH/i_am_r/lib/main.dart
+++ AppBrewery/Section3/I_AM_RICH/i_am_r/lib/main.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello World!"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("I Am Rich"),
+          backgroundColor: Colors.deepPurple[900],
+        ),
+        backgroundColor: Colors.deepPurple[600],
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"),
+          ),
+        ),
       ),
     ),
   );
