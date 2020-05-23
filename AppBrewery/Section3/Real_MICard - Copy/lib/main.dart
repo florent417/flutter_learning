24b1141b73fee0350f6fafc9a69becf9fc3dc666
florent417 <flole@live.dk>
2020-05-23 10:29:12 +0200

    added all working projects

--- AppBrewery/Section3/Real_MICard - Copy/lib/main.dart
+++ AppBrewery/Section3/Real_MICard - Copy/lib/main.dart
@@ -12,10 +12,14 @@ class MICard extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
             child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             CircleAvatar(
-              radius: 50.0,
+              radius: 75.0,
               backgroundImage: AssetImage("images/florent.jpg"),
+            ),
+            SizedBox(
+              height: 30,
             ),
             Text(
               "Florent Avdijaj",
@@ -27,7 +31,7 @@ class MICard extends StatelessWidget {
               ),
             ),
             Text(
-              "STUDENT",
+              "SWT-ENGINEERING STUDENT",
               style: TextStyle(
                 fontFamily: "SourceSansPro",
                 color: Colors.teal.shade200,
@@ -35,7 +39,52 @@ class MICard extends StatelessWidget {
                 letterSpacing: 2.5,
                 fontWeight: FontWeight.bold,
               ),
-            )
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 300.0,
+              child: Divider(
+                color: Colors.teal.shade100,
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal.shade900,
+                ),
+                title: Text(
+                  "+45 42 19 94 42",
+                  style: TextStyle(
+                      letterSpacing: 2.5,
+                      color: Colors.teal.shade900,
+                      fontFamily: "SourceSansPro",
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.teal.shade900,
+                ),
+                title: Text(
+                  "flole@live.dk",
+                  style: TextStyle(
+                      letterSpacing: 2.5,
+                      color: Colors.teal.shade900,
+                      fontFamily: "SourceSansPro",
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
           ],
         )),
       ),
