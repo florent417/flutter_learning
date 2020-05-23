e6de98c629be1e8fba90277c06f7dcf400568336
florent417 <flole@live.dk>
2020-05-23 11:02:55 +0200

    8 ball challenge done

--- AppBrewery/Section3/Magic8Ball - Kopi/lib/main.dart
+++ AppBrewery/Section3/Magic8Ball - Kopi/lib/main.dart
@@ -1,7 +1,47 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+import 'package:flutter/widgets.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text("Ask me anything"),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballPredNum = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            ballPredNum = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset("images/ball$ballPredNum.png"),
+      ),
+    );
+  }
+}
